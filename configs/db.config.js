829b4df42742d284f4373d6f66213b5b2a26bb03
Tanmoy Manna <tanmoy.manna@webskitters.com>
2024-08-10 20:54:41 +0530

    db config change

--- configs/db.config.js
+++ configs/db.config.js
@@ -1,4 +1,4 @@
 module.exports = {
     // DB_URL: "mongodb://127.0.0.1:27017/mms"
-    DB_URL : "mongodb+srv://testuser:HJYZjIV94OzZiDiC@testing.tdsyy.mongodb.net/mms?retryWrites=true&w=majority&appName=testing/mms"
+    DB_URL : "mongodb+srv://admin:tSukjHTOnntGJdYj@mms.7q1x3.mongodb.net/mms?retryWrites=true&w=majority&appName=MMS"
 }
