5f270fe2637eca4881905c227090f598879dcc41
Tanmoy Manna <tanmoy.manna@webskitters.com>
2024-08-21 01:13:20 +0530

    for live

--- configs/file.config.js
+++ configs/file.config.js
@@ -1,15 +1,15 @@
 module.exports = {
     // For Live
-    // renderUrl : 'app/uploads',
-    // deleteUrl : './app/uploads/',
-    // serviceUrl : './app/uploads/services',
-    // manufacturerUrl : './app/uploads/manufacturers'
+    renderUrl : 'app/uploads',
+    deleteUrl : './app/uploads/',
+    serviceUrl : './app/uploads/services',
+    manufacturerUrl : './app/uploads/manufacturers'
     
 
     // For Local
-    renderUrl : 'uploads',
-    deleteUrl : './uploads/',
-    serviceUrl : './uploads/services',
-    manufacturerUrl : './uploads/manufacturers'
+    // renderUrl : 'uploads',
+    // deleteUrl : './uploads/',
+    // serviceUrl : './uploads/services',
+    // manufacturerUrl : './uploads/manufacturers'
     
 }
