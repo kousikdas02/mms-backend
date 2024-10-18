6a739a2ed424852fd400e35ce2007920445f9594
Tanmoy Manna <tanmoy.manna@webskitters.com>
2024-10-18 06:09:12 +0530

    initial

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,9 @@
 function registerRoutes(app) {
     require("./auth.route")(app);
     require("./service.route")(app);
-    require("./manufacturer.route")(app);
-    require("./model.route")(app);
-    require("./engine.route")(app);
+    // require("./manufacturer.route")(app);
+    // require("./model.route")(app);
+    // require("./engine.route")(app);
     require("./booking.route")(app);
     require("./contact.route")(app);
     require("./cart.route")(app);
